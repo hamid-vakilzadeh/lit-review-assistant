107230f49de0d045524a6d7f3c8b3caa365ec009
Hamid <vakilzas@uww.edu>
2023-07-25 17:26:03 -0500

    added retry
    Took 3 minutes

--- tools/ai.py
+++ tools/ai.py
@@ -1,7 +1,9 @@
 import requests
 import streamlit as st
+from tenacity import retry, wait_random_exponential, stop_after_attempt
 
 
+@retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
 def ai_completion(
         messages: list,
         max_tokens: int = 400,
