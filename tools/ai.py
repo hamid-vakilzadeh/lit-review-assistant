31287eaf28ac7e78a63d34c1845ab28231825c37
Hamid <vakilzas@uww.edu>
2023-07-25 17:12:43 -0500

    cleaned and added comments
    Took 6 minutes

--- tools/ai.py
+++ tools/ai.py
@@ -8,24 +8,36 @@ def ai_completion(
         temperature: float = 0.3,
         model: str = "anthropic/claude-v1-100k",
         stream: bool = True,
-
 ):
+    """
+    Sends a request to the OpenRouter API to generate a response to the given
+    messages.
+
+    :param messages: A list of messages to send to the API.
+    :param max_tokens: The maximum number of tokens to generate.
+    :param temperature: The temperature to use when generating tokens.
+    :param model: The model to use when generating tokens.
+    :param stream: Whether to stream the response.
+    :return: The response from the API.
+    """
+
     headers = {
-        'Accept': 'text/event-stream',
+        'Accept': 'text/event-stream',  # This is the key to streaming
         'Authorization': 'Bearer ' + st.secrets['OPENROUTER_API_KEY'],
         'HTTP-Referer': st.secrets['OPENROUTER_REFERRER'],
         'X-Title': st.secrets['APP_TITLE'],
     }
 
     body = {
-        'model': model,  # Optional (user controls the default)
+        'model': model,
         'messages': messages,
         'max_tokens': max_tokens,
         'temperature': temperature,
         'top_p': 1,
-        'stream': stream,  # Optional (user controls the default)
+        'stream': stream,
     }
 
+    # Show a spinner while the AI is thinking
     with st.spinner(text='AI is thinking...'):
         response = requests.request(
             url="https://openrouter.ai/api/v1/chat/completions",
@@ -36,34 +48,6 @@ def ai_completion(
         )
 
     if response.status_code != 200:
-        raise "Error: Unable to get response from the server."
-    elif stream:
+        raise f"""Error: Unable to get response from the server. \n\n {response.status_code, response.text}"""
+    else:
         return response
-
-
-prompt = [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Hello!"}
-        ]
-
-"""
-response = ai_completion(
-    messages=prompt,
-    model="google/palm-2-chat-bison",
-    stream=True,
-    temperature=0.7,
-    max_tokens=400,
-)
-import json
-collected_chunks = []
-report = []
-for line in response.iter_lines():
-    if line and 'data' in line.decode('utf-8'):
-        content = line.decode('utf-8').replace('data: ', '')
-        if 'content' in content:
-            message = json.loads(content, strict=False)
-            collected_chunks.append(message)  # save the event response
-            report.append(message['choices'][0]['delta']['content'])
-            last_response = "".join(report).strip()
-            print(f'{last_response}')
-"""
