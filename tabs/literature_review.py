626f009c66819579154d171771c85e6bf00f3982
Hamid <vakilzas@uww.edu>
2023-07-28 07:55:55 -0500

    create a separate sidebar page
    Took 42 seconds

--- tabs/literature_review.py
+++ tabs/literature_review.py
@@ -7,18 +7,6 @@ from typing import Optional
 import json
 
 
-# add to notes
-def add_to_lit_review(paper):
-    # add article to lit review studies
-    st.session_state.review_pieces.append(paper)
-
-
-# remove from notes
-def remove_from_lit_review(paper):
-    # remove article from lit review studies
-    st.session_state.review_pieces.remove(paper)
-
-
 def lit_review_message(
         articles: dict,
         additional_instructions: str,
@@ -80,29 +68,6 @@ def lit_review_message(
     return messages
 
 
-def review_action_buttons(article):
-    if article not in st.session_state.review_pieces:
-        # include in lit review button
-        st.button(
-            label="✅ Include in Lit Review",
-            type="primary",
-            use_container_width=True,
-            key=f"include_{article['id']}",
-            on_click=add_to_lit_review,
-            args=(article,)
-        )
-    else:
-        # remove from lit review button
-        st.button(
-            label="❌ Remove from Lit Review",
-            type="secondary",
-            use_container_width=True,
-            key=f"remove_{article['id']}",
-            on_click=remove_from_lit_review,
-            args=(article,)
-        )
-
-
 def generate_review(articles, user_input: str):
     # create the prompt for the AI
     prompt = lit_review_message(
@@ -132,41 +97,12 @@ def generate_review(articles, user_input: str):
 
 
 def literature_review():
-    with st.sidebar:
-        st.header("📌 My Pinboard")
-        st.markdown("You can keep track of your notes, summaries, and reviews " 
-                    "that you pin while you are reviewing the literature. "
-                    )
-        # show dropdown menu to choose articles or pdfs
-        st.selectbox(
-            label="Select an option to review",
-            options=["Articles", "PDFs", "Reviews"],
-            key="articles_or_notes",
-            index=0
-        )
-        if st.session_state.articles_or_notes == "Articles":
-            st.markdown("Articles that you have found in the **Articles** tab.")
-            for article in st.session_state.pinned_articles:
-                review_action_buttons(article)
-                st.markdown(f"{article['doi'].strip()}",)
-                st.markdown(f"**{article['title']}**")
-                st.markdown(f"{article['text']}")
-                st.markdown("---")
-        elif st.session_state.articles_or_notes == "PDFs":
-            st.markdown("Summaries that you have created in the **MyPDF** tab.")
-            for piece in st.session_state.pinned_pdfs:
-                review_action_buttons(piece)
-                st.markdown(f"**{piece['citation'].strip()}**")
-                st.markdown(f"{piece['prompt']}")
-                st.markdown(f"{piece['text']}")
-                st.markdown("---")
-        else:
-            st.markdown("Reviews that you have created in the **Literature Review** tab.")
-
     with st.container():
+        # space to provide more instructions to the AI
+        st.subheader("Literature Review")
         # if notes are empty just display a message
         if len(st.session_state.review_pieces) == 0:
-            st.markdown(
+            st.warning(
                 "You have not selected anything to include in your review yet. "
                 "You can select articles and notes from the sidebar. "
                 "Use the drop down menu to switch between articles that you "
@@ -175,77 +111,62 @@ def literature_review():
                 "create as you go along in the **Literature Review** tab."
             )
 
-        else:
-            st.subheader(f"Included Pieces: {len(st.session_state.review_pieces)}")
-
-    st.markdown("---")
-
-    # space to provide more instructions to the AI
-    with st.container():
-        st.subheader("Literature Review")
         st.markdown("You can provide additional instructions for the AI. "
                     "For example, you can tell the AI to focus on a particular theme, "
                     "or to focus on a particular point of tension.\n\n"
-                    "**Note:** The AI will may not follow all your instructions. ")
+                    "**Note:** The AI may not follow all your instructions. ")
 
-        # streamlit textbox with hidden label
-        user_input = st.text_area(
-            label="You: ",
-            placeholder="Optional: If you want to provide additional instructions "
-                        "to the AI, you can do so here.",
-            key="input",
-            height=200,
-            label_visibility='collapsed'
-        )
+        with st.form(key="lit_review_form"):
+            # streamlit textbox with hidden label
+            user_input = st.text_area(
+                label="Additional Instructions",
+                placeholder="Optional: If you want to provide additional instructions "
+                            "to the AI, you can do so here.",
+                key="input",
+                height=200,
+                label_visibility='collapsed'
+            )
 
-        # enable the submit button
-        st.session_state.lit_review_submit_state = False
+            # 3 columns
+            s1, s2, s3 = st.columns(3)
 
-        # disable the submit button if there are no notes
-        if len(st.session_state.review_pieces) == 0:
-            st.session_state.lit_review_submit_state = True
-
-    # submit button along with handling errors for token limits
-    with st.container():
-
-        # current number of tokens
-        current_tokens = 0
-
-        # submit button
-        st.button(
-            label="Submit",
-            type="primary",
-            key="lit_review_submit",
-            disabled=st.session_state.lit_review_submit_state,
-            use_container_width=True
-        )
+            with s2:
+                # submit button
+                lit_review_submit = st.form_submit_button(
+                    label="Submit",
+                    type="primary",
+                    use_container_width=True
+                )
 
     st.subheader("AI Response")
     # show the previous reviews in expanders
     # Show nothing if no response is generated
-    if st.session_state.lit_review_submit:
-        ai_response = st.empty()
-        # stream the summary in the box
-        with ai_response:
-            for response_chunk in generate_review(
-                    articles=st.session_state.review_pieces,
-                    user_input=user_input
-            ):
-                ai_response.markdown(f'{response_chunk}')
+    if lit_review_submit:
+        if len(st.session_state.review_pieces) == 0:
+            st.error("You have not selected any articles or notes to include in your review.")
 
-        # create a timestamp variable
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            ai_response = st.empty()
+            # stream the summary in the box
+            with ai_response:
+                for response_chunk in generate_review(
+                        articles=st.session_state.review_pieces,
+                        user_input=user_input
+                ):
+                    ai_response.markdown(f'{response_chunk}')
 
-        # add the last review to the previous reviews with the timestamp
-        st.session_state.previous_reviews.append(
-            {'id': timestamp,
-             'doc': st.session_state.last_review,
-             'doi': [article['doi'] for article in st.session_state.review_pieces]
-             }
-        )
+            # create a timestamp variable
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        # show navigation buttons
-        #review_action_buttons(st.session_state.previous_reviews[-1])
+            # add the last review to the previous reviews with the timestamp
+            st.session_state.previous_reviews.append(
+                {'id': timestamp,
+                 'doc': st.session_state.last_review,
+                 'doi': [article['doi'] for article in st.session_state.review_pieces]
+                 }
+            )
+
+            # show navigation buttons
 
     else:
         if len(st.session_state.previous_reviews) == 0:
@@ -260,9 +181,6 @@ def literature_review():
                 )
                 # show the last review in markdown
                 st.markdown(st.session_state.previous_reviews[-1]['doc'])
-            #with st.container():
 
-                # show navigation buttons
-                # review_action_buttons(st.session_state.previous_reviews[-1])
 
     # st.write(st.session_state.review_pieces)
--- tabs/sidebar.py
+++ tabs/sidebar.py
@@ -1,27 +1,34 @@
 import streamlit as st
+from utils.funcs import show_pin_buttons
 
 
-def review_action_buttons(article):
-    if article not in st.session_state.review_pieces:
-        # include in lit review button
-        st.button(
-            label="✅ Include in Lit Review",
-            type="primary",
-            use_container_width=True,
-            key=f"include_{article['id']}",
-            on_click=add_to_lit_review,
-            args=(article,)
-        )
-    else:
-        # remove from lit review button
-        st.button(
-            label="❌ Remove from Lit Review",
-            type="secondary",
-            use_container_width=True,
-            key=f"remove_{article['id']}",
-            on_click=remove_from_lit_review,
-            args=(article,)
-        )
+def review_action_buttons(article, state_var):
+    # create 2 columns for the buttons
+    col1, col2 = st.columns(2)
+    with col1:
+        if article not in st.session_state.review_pieces:
+            # include in lit review button
+            st.button(
+                label="✅ Include in Lit Review",
+                type="primary",
+                use_container_width=True,
+                key=f"include_{article['id']}",
+                on_click=add_to_lit_review,
+                args=(article,)
+            )
+        else:
+            # remove from lit review button
+            st.button(
+                label="❌ Remove from Lit Review",
+                type="secondary",
+                use_container_width=True,
+                key=f"remove_{article['id']}",
+                on_click=remove_from_lit_review,
+                args=(article,)
+            )
+    with col2:
+        # show button for unpinning
+        show_pin_buttons(article, state_var)
 
 
 # add to notes
@@ -40,9 +47,16 @@ def show_sidebar():
     # sidebar
     with st.sidebar:
         st.header("📌 My Pinboard")
-        st.markdown("You can keep track of your notes, summaries, and reviews "
+        st.markdown("You can keep track of abstract, summaries, and reviews "
                     "that you pin while you are reviewing the literature. "
                     )
+        st.write(f"Pinned Abstracts: **{len(st.session_state.pinned_articles)}**")
+        st.write(f"Pinned PDF pieces: **{len(st.session_state.pinned_pdfs)}**")
+        st.write(f"Pinned Reviews: **{len(st.session_state.previous_reviews)}**")
+
+        st.markdown("You can also select pieces to include in your literature review.")
+        st.subheader(f"Selected pieces for review: {len(st.session_state.review_pieces)}")
+
         # show dropdown menu to choose articles or pdfs
         st.selectbox(
             label="Select an option to review",
@@ -50,19 +64,20 @@ def show_sidebar():
             key="articles_or_notes",
             index=0
         )
+
         if st.session_state.articles_or_notes == "Articles":
             st.markdown("Articles that you have found in the **Articles** tab.")
             for article in st.session_state.pinned_articles:
-                review_action_buttons(article)
-                st.markdown(f"{article['doi'].strip()}" ,)
                 st.markdown(f"**{article['title']}**")
+                review_action_buttons(article, st.session_state.pinned_articles)
+                st.markdown(f"{article['doi'].strip()}" ,)
                 st.markdown(f"{article['text']}")
                 st.markdown("---")
         elif st.session_state.articles_or_notes == "PDFs":
             st.markdown("Summaries that you have created in the **MyPDF** tab.")
             for piece in st.session_state.pinned_pdfs:
-                review_action_buttons(piece)
                 st.markdown(f"**{piece['citation'].strip()}**")
+                review_action_buttons(piece, st.session_state.pinned_pdfs)
                 st.markdown(f"{piece['prompt']}")
                 st.markdown(f"{piece['text']}")
                 st.markdown("---")
