4436205f006be1cdcdb91171d35913ca6374945d
Hamid <vakilzas@uww.edu>
2023-10-09 09:30:26 -0500

    add to context function
    Took 49 seconds

--- utils/funcs.py
+++ utils/funcs.py
@@ -35,3 +35,25 @@ def show_pin_buttons(piece, state_var):
             on_click=unpin_piece,
             args=(piece, state_var,)
         )
+
+
+def add_to_context(articles):
+    # add the article to the context
+    selected_articles = []
+    for article in articles:
+        if 'authors' in article and 'year' in article:
+            selected_articles.append(
+                f"**From {article['authors']}, {article['year']}** ({article['doi']}): {article['text']}"
+            )
+        else:
+            selected_articles.append(
+                f"**From {article['citation']}** ({article['doi']}): {article['text']}"
+            )
+
+    selected_articles = "\n\n ".join(selected_articles)
+    st.session_state.messages_to_interface.append({"role": "user", "content": selected_articles})
+    st.session_state.messages_to_api.append({"role": "user", "content": selected_articles})
+    st.session_state.pinned_articles = []
+    st.session_state.article_search_results = []
+    st.session_state.pinned_pdfs = []
+    st.toast(f"**added successfully!**", icon="📌")
