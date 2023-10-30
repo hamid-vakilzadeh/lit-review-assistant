b323558d877105cd9a8ae1812dbb6fec6d9977d3
Hamid <vakilzas@uww.edu>
2023-10-30 15:59:30 -0500

    Fix typos
    Took 5 minutes

--- utils/doi.py
+++ utils/doi.py
@@ -83,8 +83,9 @@ def get_article_with_doi(doi: str) -> dict:
             'type': 'abstract'
         }
         return article_info
-    except:
-        raise Exception('Could not find this DOI. If this is a recent paper, the database may not have it yet.')
+    except Exception as e:
+        raise e
+        # raise Exception('Could not find this DOI. If this is a recent paper, the database may not have it yet.')
 
 
 if __name__ == '__main__':
