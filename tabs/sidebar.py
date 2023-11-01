ee9eaa7dc8a58c8fc811dcf795358e6640389726
Hamid <vakilzas@uww.edu>
2023-11-01 11:31:49 -0500

    fix the side bar
    Took 29 seconds

--- tabs/sidebar.py
+++ tabs/sidebar.py
@@ -1,49 +1,7 @@
 import streamlit as st
-from utils.funcs import show_pin_buttons
+from utils.funcs import review_action_buttons
 from time import time
 from utils.firestore_db import new_user_request
-
-def review_action_buttons(article, state_var):
-    # create 2 columns for the buttons
-    col1, col2 = st.columns(2)
-    with col1:
-        if article not in st.session_state.review_pieces:
-            # include in lit review button
-            st.button(
-                label="✅ Include in Lit Review",
-                type="primary",
-                use_container_width=True,
-                key=f"include_{article['id']}",
-                on_click=add_to_lit_review,
-                args=(article,)
-            )
-        else:
-            # remove from lit review button
-            st.button(
-                label="❌ Remove from Lit Review",
-                type="secondary",
-                use_container_width=True,
-                key=f"remove_{article['id']}",
-                on_click=remove_from_lit_review,
-                args=(article,)
-            )
-    with col2:
-        # show button for unpinning
-        show_pin_buttons(article, state_var)
-
-
-# add to notes
-def add_to_lit_review(paper):
-    # add article to lit review studies
-    st.session_state.review_pieces.append(paper)
-    st.toast(f"**Added to 📚 literature review!**", icon="✅")
-
-
-# remove from notes
-def remove_from_lit_review(paper):
-    # remove article from lit review studies
-    st.session_state.review_pieces.remove(paper)
-    st.toast(f"**Removed from 📚 literature review!**", icon="❌")
 
 
 def show_login():
@@ -184,7 +142,6 @@ def show_sidebar():
             options=[
                 f"Abstracts: {len(st.session_state.pinned_articles)}",
                 f"PDF pieces: {len(st.session_state.pinned_pdfs)}",
-                f"Reviews: {len(st.session_state.pinned_reviews)}"
             ],
             key="show_pinned",
             horizontal=True,
@@ -206,12 +163,4 @@ def show_sidebar():
                 review_action_buttons(piece, st.session_state.pinned_pdfs)
                 st.markdown(f"{piece['prompt']}")
                 st.markdown(f"{piece['text']}")
-                st.markdown("---")
-        else:
-            st.markdown("Reviews that you have created in the **Literature Review** tab.")
-            for review in st.session_state.pinned_reviews:
-                st.markdown(f"**{review['id']}**")
-                review_action_buttons(review, st.session_state.pinned_reviews)
-                st.markdown(f"{review['text']}")
-                st.markdown("{citations}".format(citations='\n'.join(review['citation'])))
                 st.markdown("---")
