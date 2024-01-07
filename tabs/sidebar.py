26c3eee8365066952ee86b06b29c2b01ec78dcf6
Hamid <vakilzas@uww.edu>
2024-01-07 15:05:41 -0600

    remove unpopular models.
    Took 8 hours 34 minutes

--- tabs/sidebar.py
+++ tabs/sidebar.py
@@ -103,32 +103,23 @@ def choose_model():
     chosen_model = st.selectbox(
         label='Model Name',
         options=[
-            'OpenAI: GPT-3.5 16K',
             'OpenAI: GPT-3.5 16K (Latest)',
-            'OpenAI: GPT-4 32K',
             'OpenAI: GPT-4 128K (Latest)',
-            'Anthropic: Claude v2',
-            'Anthropic: Claude Instant v1',
-            'Meta: Llama v2 13B Chat',
+            'Anthropic: Claude v2.1 200K',
             'Meta: Llama v2 70B Chat',
+      #       'Google: Gemini Pro',
                  ],
     )
-    if chosen_model == 'OpenAI: GPT-3.5 16K':
-        st.session_state.selected_model = 'openai/gpt-3.5-turbo-16k'
     if chosen_model == 'OpenAI: GPT-3.5 16K (Latest)':
         st.session_state.selected_model = 'openai/gpt-3.5-turbo-1106'
-    if chosen_model == 'OpenAI: GPT-4 32K':
-        st.session_state.selected_model = 'openai/gpt-4-32k'
     if chosen_model == 'OpenAI: GPT-4 128K (Latest)':
         st.session_state.selected_model = 'openai/gpt-4-1106-preview'
-    if chosen_model == 'Anthropic: Claude v2':
+    if chosen_model == 'Anthropic: Claude v2.1 200K':
         st.session_state.selected_model = 'anthropic/claude-2'
-    if chosen_model == 'Anthropic: Claude Instant v1':
-        st.session_state.selected_model = 'anthropic/claude-instant-v1'
-    if chosen_model == 'Meta: Llama v2 13B Chat':
-        st.session_state.selected_model = 'meta-llama/llama-2-13b-chat'
     if chosen_model == 'Meta: Llama v2 70B Chat':
         st.session_state.selected_model = 'meta-llama/llama-2-70b-chat'
+    # if chosen_model == 'Google: Gemini Pro':
+    #     st.session_state.selected_model = 'google/gemini-pro'
 
 
 def show_sidebar():
