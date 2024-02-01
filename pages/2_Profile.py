4703057df3de538cbbb120395450325e92f7551b
Hamid <vakilzas@uww.edu>
2024-02-01 08:51:57 -0600

    fix the color theme.
    Took 10 seconds

--- pages/2_Profile.py
+++ pages/2_Profile.py
@@ -1,10 +1,12 @@
 from tabs import profile, sidebar
 from utils.session_state_vars import ensure_session_state_vars
 import streamlit as st
-
+from tabs.css import css_code
 # ensure the session state variables are created
 ensure_session_state_vars()
 
+# run the css
+css_code()
 
 if __name__ == '__main__':
     if 'user' not in st.session_state:
