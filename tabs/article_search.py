d7dbaaf661d05297df9a3b6b4083d4a1dec1314a
Hamid <vakilzas@uww.edu>
2024-07-30 16:35:45 -0500

    add advanced search
    Took 43 seconds

--- tabs/article_search.py
+++ tabs/article_search.py
@@ -6,6 +6,11 @@ from utils.doi import get_apa_citation
 from utils.funcs import pin_piece, unpin_piece
 from pandas import read_csv
 import json
+import requests
+import pandas as pd
+from datetime import datetime
+from utils.session_state_vars import column_order, column_config
+from tabs.css import css_code
 
 
 @st.cache_data
@@ -16,7 +21,6 @@ def get_journal_names():
 def prep_gpt_summary(
         document,
 ) -> list:
-
     messages = [
         {
             "role": "system",
@@ -104,6 +108,7 @@ def update_filter():
 
 @st.experimental_fragment
 def article_search():
+    css_code()
     st.subheader("Article Search")
     # user input for topic to search for
     # explain
@@ -167,7 +172,6 @@ def article_search():
             # )
 
             # 3 columns for submit form button
-
 
             st.text_input(
                 label="**Search a Topic**",
@@ -355,3 +359,209 @@ def article_search():
                 st.markdown('---')
 
                 # st.write(st.session_state)
+
+
+def semantic_bulk_search(
+        search_term: str = "",
+        year_start: datetime = "",
+        year_end: datetime = "",
+        at_least_citations: int = 0,
+        venues: list = None,
+        token: str = None,
+        sort_by: str = 'citationCount:desc',
+):
+    venues_list = None
+    if venues:
+        venues_list = ','.join(venues)
+    if year_end == datetime.now():
+        year_end = ''
+
+    parameters = {'query': search_term,
+                  'publicationDateOrYear': f"{year_start}:{year_end}",
+                  'minCitationCount': at_least_citations,
+                  'venue': venues_list,
+                  'fieldsOfStudy': 'Business',
+                  'fields': 'title,authors,venue,year,citationCount,s2FieldsOfStudy,abstract,externalIds',
+                  'sort': sort_by,
+                  }
+    if token:
+        parameters['token'] = token
+    response = requests.get(
+        url=f"https://api.semanticscholar.org/graph/v1/paper/search/bulk",
+        headers={'x-api-key': st.secrets['semantic_scholar']},
+        params=parameters
+    )
+    if response.status_code != 200:
+        return None
+    return response.json()
+
+
+def get_external_url(x):
+    if "DOI" in x:
+        return f"https://doi.org/{x['DOI']}"
+    elif "ArXiv" in x:
+        return f"https://arxiv.org/abs/{x['ArXiv']}"
+    elif "PMID" in x:
+        return f"https://pubmed.ncbi.nlm.nih.gov/{x['PMID']}"
+    elif "MAG" in x:
+        return f"https://academic.microsoft.com/paper/{x['MAG']}"
+    elif "DBLP" in x:
+        return f"https://dblp.org/rec/{x['DBLP']}"
+    elif "CorpusId" in x:
+        return f"https://www.semanticscholar.org/paper/{x['CorpusId']}"
+    else:
+        return None
+
+
+@st.cache_data(show_spinner=False)
+def get_venues():
+    data = pd.read_json('database/venues.json', lines=True)
+    return data[data['type'] == 'journal']['name'].tolist()
+
+
+def bulk_search():
+    with st.form(key="advanced_search"):
+        st.subheader("Advanced Search")
+        search_term = st.text_input(
+            label="**Search**",
+            placeholder="search for a topic",
+            key="semantic_bulk_search",
+            label_visibility="collapsed",
+        )
+        sort_col, journal_col = st.columns(2)
+        sort_options = {
+            "Publication Date (Newest to Oldest)": "publicationDate:desc",
+            "Publication Date (Oldest to Newest)": "publicationDate:asc",
+            "Citation Count (Highest to Lowest)": "citationCount:desc",
+            "Citation Count (Lowest to Highest)": "citationCount:asc",
+        }
+        sort_by = sort_col.selectbox(
+            label="Sort By",
+            options=sort_options.keys(),
+            key="sort_by",
+        )
+
+        journals = journal_col.multiselect(
+            label="Journals",
+            options=get_venues(),
+            key='journals',
+            disabled=False
+        )
+        col1, col2, col3 = st.columns(3)
+        date_start = col1.date_input(
+            label="Date Start",
+            min_value=datetime(1950, 1, 1),
+            max_value=datetime.now(),
+            value=datetime(2000, 1, 1),
+            key="date_start"
+        )
+
+        date_end = col2.date_input(
+            label="Date End",
+            min_value=date_start,
+            max_value=datetime.now(),
+            value=datetime.now(),
+            key="date_end"
+        )
+
+        at_least_citations = col3.number_input(
+            label="Cited at least",
+            min_value=0,
+            value=0,
+            step=1,
+            key="at_least_citations"
+        )
+
+        search_button = st.form_submit_button(
+            label="Search",
+            type="primary",
+            use_container_width=False,
+            on_click=lambda: st.session_state.pop('bulk_search_response', None)
+        )
+
+    if search_button and search_term.strip() != "":
+        st.session_state.bulk_search_response = semantic_bulk_search(
+            search_term=search_term,
+            sort_by=sort_options[sort_by],
+            year_start=date_start,
+            year_end=date_end,
+            at_least_citations=at_least_citations,
+            venues=journals
+        )
+    elif search_button and search_term.strip() == "":
+        st.session_state.total_results = 0
+        st.warning("Please enter a search term.")
+
+    if 'bulk_search_response' in st.session_state:
+        st.session_state.total_results = st.session_state.bulk_search_response['total']
+        st.session_state.bulk_search_token = st.session_state.bulk_search_response['token']
+        data_to_show = pd.DataFrame(st.session_state.bulk_search_response.copy()['data'])
+        if len(data_to_show) == 0:
+            st.warning("No results found.")
+        else:
+            data_to_show['s2FieldsOfStudyUnique'] = data_to_show.s2FieldsOfStudy.apply(
+                lambda x: list({d['category'] for d in x} if x else []))
+            data_to_show['authorNames'] = data_to_show['authors'].apply(
+                lambda x: [d['name'] for d in x])
+            data_to_show = data_to_show.drop(columns=['s2FieldsOfStudy', 'authors'])
+            data_to_show.set_index('paperId', inplace=True)
+            data_to_show['Source'] = data_to_show['externalIds'].apply(get_external_url)
+            st.session_state.data_to_show = data_to_show.copy()
+            st.session_state.pop('bulk_search_response', None)
+
+    if 'data_to_show' in st.session_state:
+        return_col, next_col = st.columns(2, gap='large')
+        if len(st.session_state.data_to_show) == 1000:
+            st.success("More than a 1000 results found. Try to narrow down your search.")
+        else:
+            st.success(f"{len(st.session_state.data_to_show)} results found.")
+        st.write('Select up to 20 papers to add to your chat.')
+        results = st.dataframe(
+            hide_index=True,
+            data=st.session_state.data_to_show,
+
+            column_order=column_order(),
+            column_config=column_config(),
+            selection_mode="multi-row",
+            on_select='rerun',
+            use_container_width=True,
+
+        )
+
+        papers = results.selection.rows
+        disabled = False
+        if len(papers) > 20:
+            disabled = True
+            st.error("You can only select up to 20 papers at a time.")
+
+        return_col.button(
+            label='Finish and Return to Chat',
+            type='primary',
+            key='close_advanced_search',
+            use_container_width=True,
+            disabled=disabled
+        )
+
+        selected_papers = st.session_state.data_to_show.iloc[papers]
+
+        if st.session_state.close_advanced_search:
+            st.session_state.pinned_articles_ss = \
+                pd.concat([st.session_state.pinned_articles_ss, selected_papers])
+            st.session_state.pinned_articles_ss.reset_index(inplace=True)
+            st.session_state.pinned_articles_ss.drop_duplicates(
+                subset=['paperId'],
+                inplace=True)
+            st.session_state.pinned_articles_ss.set_index('paperId', inplace=True)
+
+            # add_pd_to_lit_review(st.session_state.pinned_articles_ss)
+            st.rerun()
+
+
+def advanced_search():
+    css_code()
+    bulk_search_tab, author_search_tab, paper_search_tab = st.tabs(
+        ["Topic Search", "Author Search", "Paper information"]
+    )
+    with bulk_search_tab:
+        bulk_search()
+    # st.write(st.session_state)
