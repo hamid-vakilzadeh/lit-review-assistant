44aa09f64400f53dd23e3b1fd103da8af9c72f49
Hamid Vakilzadeh <vakilzas@uww.edu>
2025-06-04 11:43:59 -0500

    fix: update default model to gpt-3.5-turbo in num_tokens_from_messages function

--- utils/openai_api.py
+++ utils/openai_api.py
@@ -51,7 +51,7 @@ def chat_completion(
 
 def num_tokens_from_messages(
         messages: list,
-        model: str = "gpt-3.5-turbo-0301"
+        model: str = "gpt-3.5-turbo"
 ):
     """
     Returns the number of tokens used by a list of messages.
@@ -65,12 +65,12 @@ def num_tokens_from_messages(
         print("Warning: model not found. Using cl100k_base encoding.")
         encoding = tiktoken.get_encoding("cl100k_base")
     if model in ["gpt-3.5-turbo", "gpt-3.5-turbo-16k"]:
-        print("Warning: gpt-3.5-turbo may change over time. Returning num tokens assuming gpt-3.5-turbo-0301.")
-        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301")
+        print("Warning: gpt-3.5-turbo may change over time. Returning num tokens assuming gpt-3.5-turbo.")
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo")
     elif model in ["gpt-4", "gpt-4-32k"]:
         print("Warning: gpt-4 may change over time. Returning num tokens assuming gpt-4-0314.")
         return num_tokens_from_messages(messages, model="gpt-4-0314")
-    elif model == "gpt-3.5-turbo-0301":
+    elif model == "gpt-3.5-turbo":
         tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
         tokens_per_name = -1  # if there's a name, the role is omitted
     elif model == "gpt-4-0314":
